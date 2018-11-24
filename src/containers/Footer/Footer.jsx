e44afe0d8196f4ff21bbacb1727b31c71bd45781
Leon <ierehon1905@gmail.com>
2018-11-24 23:32:37 +0300

    structure draft

--- src/containers/Footer/Footer.jsx
+++ src/containers/Footer/Footer.jsx
@@ -1,7 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const FooterContents = styled.div`
+  background-color: burlywood;
+  min-height: 60px;
+`;
 
 const Footer = () => {
-    return ( <p>footer</p> );
-}
- 
+  return <FooterContents />;
+};
+
 export default Footer;
--- src/containers/Methods/Methods.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-class Methods extends Component {
-    render() { 
-        return ( <p>choose ur travel way</p> );
-    }
-}
- 
-export default Methods;
