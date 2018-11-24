454870c0faa16a0e61cd7cce08bd5ba59050ac8d
Leon <ierehon1905@gmail.com>
2018-11-24 23:32:37 +0300

    structure draft

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Home from "./Home/home";
-import Methods from "./Methods/Methods";
+import RouteSearch from "./RouteSearch/RouteSearch";
 
 class App extends Component {
   render() {
@@ -11,8 +11,8 @@ class App extends Component {
       <div>
         <Header />
         <Route path="/home" component={Home} />
-        <Route path="/methods" component={Methods} />
         <Route exact path="/" render={() => <Redirect to="/home" />} />
+        <Route path='/routesearch' component={RouteSearch}></Route>
         <Footer />
       </div>
     );
