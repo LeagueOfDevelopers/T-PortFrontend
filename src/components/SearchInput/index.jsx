bec436f50e12afee1c7131f64c9111f92d63cd4e
Leon <ierehon1905@gmail.com>
2018-12-03 23:42:51 +0300

    home page improvement

--- src/components/SearchInput/index.jsx
+++ src/components/SearchInput/index.jsx
@@ -2,15 +2,19 @@ import React from "react";
 import styled from "styled-components";
 
 const StyledSearchInput = styled.input`
-  display: inline-block;
-  background-color: red;
+  /* display: inline-block; */
+  /* background-color: red; */
+  /* padding: 10px; */
+  width: 100%;
   border: 0;
-  padding: 1;
-  width: 40px;
+  /* height: ; */
+  font-size: 1.5em;
+  border-bottom: 1px solid #f3efef;
+  /* border-radius: 1em; */
 `;
 
-const SearchInput = () => {
-  return <StyledSearchInput />;
+const SearchInput = (props) => {
+  return <StyledSearchInput type={props.type}/>;
 };
 
 export default SearchInput;
